13671dac870a48e1d6006a7d58faf0c37a73bd15
ifi999 <kkwkjm4231@gmail.com>
2023-06-10 12:13:01 +0900

    consumer 모듈 생성

---
+++ consumer/src/test/java/com/ifi/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifi.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
